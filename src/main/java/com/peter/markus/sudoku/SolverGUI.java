9c8c6946b227d3d9b561f7e8ee9e6556960465c6
Markus Peter <markus.peter@sap.com>
2022-10-06 14:57:38 +0200

    Changed window title

--- src/main/java/com/peter/markus/sudoku/SolverGUI.java
+++ src/main/java/com/peter/markus/sudoku/SolverGUI.java
@@ -41,7 +41,7 @@ public class SolverGUI extends Application {
         
         Scene scene = new Scene(frame, 600, 600);
         scene.getStylesheets().add("game.css");
-		stage.setTitle("Sudoku Solver");
+		stage.setTitle("Sudoku Puzzle Solver");
         stage.setScene(scene);
         stage.show();
         this.stage = stage;
